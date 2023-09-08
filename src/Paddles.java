47c9ff8bd0095e62d7318913b438e837604b8521
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-08 09:22:27 -0700

    methods setup for paddles

--- src/Paddles.java
+++ src/Paddles.java
@@ -1,6 +1,29 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Paddles {
 
     Paddles() {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void setYDirection(int yDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
 
     }
 }
