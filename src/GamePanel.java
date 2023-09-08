ce202100c1f5523e6257b555a59e4336802e2cb0
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-08 10:06:47 -0700

    added collision

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -56,11 +56,16 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void move() {
-
+        paddle1.move();
+        paddle2.move();
     }
 
     public void checkCollision() {
+        if(paddle1.y <= 0) paddle1.y = 0;
+        if(paddle1.y >= (GAME_HEIGHT - PADDLE_HEIGHT)) paddle1.y = GAME_HEIGHT - PADDLE_HEIGHT;
 
+        if(paddle2.y <= 0) paddle2.y = 0;
+        if(paddle2.y >= (GAME_HEIGHT - PADDLE_HEIGHT)) paddle2.y = GAME_HEIGHT - PADDLE_HEIGHT;
     }
 
     public void run() {
