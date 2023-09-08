beee6cfdc8672f642305793fd2be92bf722f8fba
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-08 09:24:17 -0700

    methods added to ball class

--- src/Ball.java
+++ src/Ball.java
@@ -1,6 +1,24 @@
+import java.awt.*;
+
 public class Ball {
 
     Ball() {
+
+    }
+
+    public void setXDirection(int randomXDirection) {
+
+    }
+
+    public void setYDirection(int randomYDirection) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void draw(Graphics g) {
 
     }
 }
